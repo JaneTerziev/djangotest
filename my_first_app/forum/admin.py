cc5f59c4dc2c043acc8560452738d424519e01d0
Jane Terziev <jane.terziev@webfactory.mk>
2019-10-24 11:14:05 +0200

    Added change and delete permissions for users own posts

--- my_first_app/forum/admin.py
+++ my_first_app/forum/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Post, Comment
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = ['title', 'content']
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+    def has_change_permission(self, request, obj=None):
+        if obj is not None:
+            return is_user_owner(request.user, obj.user)
+        else:
+            return False
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is not None:
+            return is_user_owner(request.user, obj.user)
+        else:
+            return False
+
+
+def is_user_owner(request_user, obj_user):
+    return request_user == obj_user
--- my_first_app/forum/models.py
+++ my_first_app/forum/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=1000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField(max_length=1000)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
